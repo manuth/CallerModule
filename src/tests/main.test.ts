75efc4911d20054d21ce0798ea78b65220f32d42
Manuel Thalmann <m@nuth.ch>
2018-12-09 14:16:11 +0100

    Fix files according to tslint

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -1,14 +1,14 @@
-import * as assert from 'assert';
-import * as callerModule from '../main';
+import * as assert from "assert";
+import * as callerModule from "../main";
 
-describe('CallerModule', () =>
+describe("CallerModule", () =>
 {
-  it('Determining the caller of `testCallerModule`... (exceptiong `mocha`)', function testCallerModule()
+  it("Determining the caller of `testCallerModule`... (exceptiong `mocha`)", function testCallerModule()
   {
-    assert.strictEqual(callerModule.GetCallerModule(testCallerModule).name, 'mocha');
+    assert.strictEqual(callerModule.GetCallerModule(testCallerModule).name, "mocha");
   });
-  it ('Determining the caller of the current context... (excepting `caller-module`)', () =>
+  it ("Determining the caller of the current context... (excepting `caller-module`)", () =>
   {
-    assert.strictEqual(callerModule.GetCallerModule().name, 'caller-module');
+    assert.strictEqual(callerModule.GetCallerModule().name, "caller-module");
   });
 });
